b351073367b306588aa241ac81651eade874a712
roshani-gaund <roshanigaund0586@gmail.com>
2026-01-12 12:36:58 +0530

    first commit

--- Frontend/src/util/api.js
+++ Frontend/src/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:" https://chat-app-pif3.onrender.com",
+  baseURL:" https://chat-app-pif3.onrender.com/api",
   withCredentials: true,
 });
 export const BASE_URL = " https://chat-app-pif3.onrender.com";
