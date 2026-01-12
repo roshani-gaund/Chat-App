3ca4f28ff8c892932ba8e233e24a083260e44ea9
roshani-gaund <roshanigaund0586@gmail.com>
2026-01-12 13:39:45 +0530

    api in cors

--- Frontend/src/util/api.js
+++ Frontend/src/util/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:" https://chat-app-pif3.onrender.com/api",
-  withCredentials: true,
+  withCredentials:true
 });
 export const BASE_URL = " https://chat-app-pif3.onrender.com";
 
